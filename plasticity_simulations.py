880663e3c1e6d682b0b1a1bdcad60ee13afb8622
Cathy Chen <cathychen2357@gmail.com>
2017-12-17 15:20:11 -0500

    Change some parameters and reorganize.

--- plasticity_simulations.py
+++ plasticity_simulations.py
@@ -10,34 +10,58 @@ import sys
 
 import plasticity_simulations_utils as utils
 
+def run_simulation(update_method, all_inputs, update_name, nonnegative_weights):
+    num_inputs, num_timesteps = all_inputs.shape
+    learning_rate = 0.1
+    
+    initial_weight = np.random.randn(num_inputs)
+    all_weights = np.empty((num_inputs, num_timesteps + 1))
+    all_weights[:,0] = initial_weight
+    
+    for i in range(num_timesteps):
+        current_input = all_inputs[:, i]
+        old_weight = all_weights[:,i]
+        new_weight = update_method(old_weight, current_input, learning_rate)
+        if nonnegative_weights:
+            new_weight[new_weight < 0] = 0
+        all_weights[:,i + 1] = new_weight
+    
+    # Plot results without averaging.
+    plt.figure(0)
+    plt.title("Weights from %s Update" % update_name)
+    for i in range(num_inputs):
+        weight_trajectory = all_weights[i,:]
+        plt.plot(weight_trajectory, label="w%d" % i)
+        plt.legend(loc="lower left")
+        print("w%d: %f" % (i, weight_trajectory[-1]))
+
 # TODO: Add averaging?
 # TODO: Plot with multiple runs?
-def basic_hebb_simulation():
+def basic_hebb_simulation(nonnegative_weights):
     num_inputs = 2
-    num_timesteps = 100
+    num_timesteps = 1000
     all_inputs = utils.generate_inputs(num_inputs, num_timesteps)
-    utils.run_simulation(utils.basic_hebb_update, all_inputs, "Basic Hebb")
+    run_simulation(utils.basic_hebb_update, all_inputs, "Basic Hebb", nonnegative_weights)
 
-def covariance_simulation():
+def covariance_simulation(nonnegative_weights):
     num_inputs = 2
-    num_timesteps = 100
+    num_timesteps = 1000
     all_inputs = utils.generate_inputs(num_inputs, num_timesteps)
     all_inputs -= np.reshape(np.mean(all_inputs, axis=1), (num_inputs, 1))
-    utils.run_simulation(utils.basic_hebb_update, all_inputs, "Covariance")
+    run_simulation(utils.basic_hebb_update, all_inputs, "Covariance", nonnegative_weights)
     
-def subtractive_normalization_simulation():
+def subtractive_normalization_simulation(nonnegative_weights):
     num_inputs = 2
     num_timesteps = 1000
     all_inputs = utils.generate_inputs(num_inputs, num_timesteps)
-    utils.run_simulation(utils.subtractive_normalization_update, all_inputs, "Subractive Normalization")
+    run_simulation(utils.subtractive_normalization_update, all_inputs, "Subractive Normalization", nonnegative_weights)
 
-def oja_simulation():
+def oja_simulation(nonnegative_weights):
     num_inputs = 2
     num_timesteps = 1000
     all_inputs = utils.generate_inputs(num_inputs, num_timesteps)
-    utils.run_simulation(utils.oja_update, all_inputs, "Oja")
+    run_simulation(utils.oja_update, all_inputs, "Oja", nonnegative_weights)
         
-def main(argv):
-    oja_simulation()
 if __name__ == "__main__":
-    main(sys.argv)
+    nonnegative_weights = True
+    basic_hebb_simulation(nonnegative_weights)
--- plasticity_simulations_utils.py
+++ plasticity_simulations_utils.py
@@ -7,29 +7,6 @@ Created on Sun Dec 17 11:47:33 2017
 import matplotlib.pyplot as plt
 import numpy as np
 
-def run_simulation(update_method, all_inputs, update_name):
-    num_inputs, num_timesteps = all_inputs.shape
-    learning_rate = 0.1
-    
-    initial_weight = np.random.randn(num_inputs)
-    all_weights = np.empty((num_inputs, num_timesteps + 1))
-    all_weights[:,0] = initial_weight
-    
-    for i in range(num_timesteps):
-        current_input = all_inputs[:, i]
-        old_weight = all_weights[:,i]
-        new_weight = update_method(old_weight, current_input, learning_rate)
-        all_weights[:,i + 1] = new_weight
-    
-    # Plot results without averaging.
-    plt.figure(0)
-    plt.title("Weights from %s Update" % update_name)
-    for i in range(num_inputs):
-        weight_trajectory = all_weights[i,:]
-        plt.plot(weight_trajectory, label="w%d" % i)
-        plt.legend(loc="lower left")
-        print("w%d: %f" % (i, weight_trajectory[-1]))
-
 def generate_inputs(num_inputs, num_timesteps):
     all_inputs = np.random.rand(num_inputs, num_timesteps)
     return all_inputs # [0,1) so always non-negative.
@@ -49,7 +26,7 @@ def subtractive_normalization_update(weights, current_input, learning_rate):
     return weights + learning_rate * delta
     
 def oja_update(weights, current_input, learning_rate):
-    alpha = 0.1
+    alpha = 1
     activation = np.dot(weights, current_input)
     activation = max(0, activation) # Prevent negative activity.
     delta = activation * current_input - alpha * activation * activation * weights
