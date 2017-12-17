327a3da538d1c42cfcb74b0d055988f5f064d7f5
Cathy Chen <cathychen2357@gmail.com>
2017-12-17 17:32:19 -0500

    Add averaged versions and modularize.

--- ocular_dominance_simulation_single_neuron.py
+++ ocular_dominance_simulation_single_neuron.py
@@ -10,42 +10,63 @@ import matplotlib.pyplot as plt
 import numpy as np
 import plasticity_simulations_utils as utils
 
-def run_simulation_average():
-    num_timesteps = 100
-    num_inputs = 2
-    learning_rate = 1
+def run_average_simulation(num_inputs, num_timesteps, learning_rate, nonnegative_weights, update_function):
     initial_weight = np.abs(np.random.randn(num_inputs)) # Right, then left eye.
-    #all_weights = np.empty((num_inputs, num_timesteps + 1))
-    #all_weights[:,0] = initial_weight
-    Q = np.zeros((num_inputs, num_inputs))
+    all_weights = np.empty((num_inputs, num_timesteps + 1))
+    all_weights[:,0] = initial_weight
+    Q = utils.generate_Q(num_inputs, num_timesteps)
+    
     for i in range(num_timesteps):
-        shared_input = np.abs(np.random.randn())
-        input_left = shared_input + 0.1 * np.random.randn()
-        input_right = shared_input + 0.1 * np.random.randn()
-        Q[0,0] += input_right * input_right
-        Q[1,1] += input_left * input_left
-        Q[0,1] += input_left * input_right
-        Q[1,0] += input_left * input_right
-    Q = Q / num_timesteps
-    weight = initial_weight + learning_rate * np.dot(Q, initial_weight)
-    print(weight)
-    print("hi")
-    print(np.dot(Q, initial_weight))
+        weight = all_weights[:,i]
+        new_weight = update_function(weight, learning_rate, Q)
+        if nonnegative_weights:
+            new_weight[new_weight < 0] = 0
+        all_weights[:, i + 1] = new_weight
+    return all_weights
 
-# Weight trajectory with specified updates for a single eye.
-def run_simulation_trajectory(update_function, update_name, nonnegative_weights=True):
+def run_simulation_trajectory_average(update_function, update_name, nonnegative_weights=True):
     num_timesteps = 100
     num_inputs = 2
     learning_rate = 1
+    all_weights = run_average_simulation(num_inputs, num_timesteps, learning_rate, nonnegative_weights, update_function)
+
+    plt.figure()
+    weights_trajectory_left = all_weights[0,:]
+    weights_trajectory_right = all_weights[1,:]
+    plt.plot(weights_trajectory_left, label="Left eye weights")
+    plt.plot(weights_trajectory_right, label="Right eye weights")
+    plt.xlabel("Number of Timesteps")
+    plt.ylabel("Weight")
+    plt.legend()
+    plt.title("Ocular Dominance Weight Trajectories, Average %s Updates" % update_name)
+
+def run_simulation_many_trials_average(update_function, update_name, nonnegative_weights=True):
+    num_timesteps = 100
+    num_inputs = 2
+    learning_rate = 0.1
+    num_trials = 10
+    
+    final_weights = np.empty((num_inputs, num_trials))
+    for trial in range(num_trials):
+        all_weights = run_average_simulation(num_inputs, num_timesteps, learning_rate, nonnegative_weights, update_function)
+        final_weights[:, trial] = all_weights[:,-1]
+    
+    plt.figure()
+    final_weights_left = final_weights[0,:]
+    final_weights_right = final_weights[1,:]
+    plt.scatter(final_weights_left, final_weights_right)
+    plt.xlim([0, max(final_weights_left)])
+    plt.ylim([0, max(final_weights_right)])
+    plt.xlabel("Final Weights, Left Eye")
+    plt.ylabel("Final Weights, Right Eye")
+    plt.title("Ocular Dominance Final Weights, %s Updates" % update_name)
+
+def run_simulation(num_inputs, num_timesteps, learning_rate, nonnegative_weights, update_function):
     initial_weight = np.abs(np.random.randn(num_inputs)) # Right, then left eye.
     all_weights = np.empty((num_inputs, num_timesteps + 1))
     all_weights[:,0] = initial_weight
     for i in range(num_timesteps):
-        # Generate correlated inputs.
-        shared_input = np.abs(np.random.randn())
-        input_left = shared_input + 0.5 * np.random.randn()
-        input_right = shared_input + 0.5 * np.random.randn()
-        current_input = np.array([input_left, input_right])
+        current_input = utils.generate_correlated_input()
         
         # Update weights.
         weights = all_weights[:,i]
@@ -53,6 +74,14 @@ def run_simulation_trajectory(update_function, update_name, nonnegative_weights=
         if nonnegative_weights:
             new_weights[new_weights < 0] = 0
         all_weights[:, i + 1] = new_weights
+    return all_weights
+
+# Weight trajectory with specified updates for a single eye.
+def run_simulation_trajectory(update_function, update_name, nonnegative_weights=True):
+    num_timesteps = 100
+    num_inputs = 2
+    learning_rate = 1
+    all_weights = run_simulation(num_inputs, num_timesteps, learning_rate, nonnegative_weights, update_function)
         
     plt.figure()
     weights_trajectory_left = all_weights[0,:]
@@ -74,24 +103,8 @@ def run_simulation_many_trials(update_function, update_name, nonnegative_weights
     
     final_weights = np.empty((num_inputs, num_trials))
     for trial in range(num_trials):
-        initial_weight = np.abs(np.random.randn(num_inputs)) # Right, then left eye.
-        all_weights = np.empty((num_inputs, num_timesteps + 1))
-        all_weights[:,0] = initial_weight
-        for timestep in range(num_timesteps):
-            # Generate correlated inputs.
-            shared_input = np.abs(np.random.randn())
-            input_left = shared_input + 0.5 * np.random.randn()
-            input_right = shared_input + 0.5 * np.random.randn()
-            current_input = np.array([input_left, input_right])
-            
-            # Update weights.
-            weights = all_weights[:,timestep]
-            new_weights = update_function(weights, current_input, learning_rate)
-            if nonnegative_weights:
-                new_weights[new_weights < 0] = 0
-            all_weights[:, timestep + 1] = new_weights
-        final_weights[:, trial] = new_weights
-        print(new_weights)
+        all_weights = run_simulation(num_inputs, num_timesteps, learning_rate, nonnegative_weights, update_function)
+        final_weights[:, trial] = all_weights[:,-1]
     
     plt.figure()
     final_weights_left = final_weights[0,:]
@@ -105,10 +118,13 @@ def run_simulation_many_trials(update_function, update_name, nonnegative_weights
 
 # TODO: When to enforce nonnegative weights?
 if __name__ == "__main__":
-    run_simulation_many_trials(utils.basic_hebb_update, "Basic Hebb")
-    run_simulation_many_trials(utils.subtractive_normalization_update, "Subtractive Normalization")
-    run_simulation_many_trials(utils.oja_update, "Oja", nonnegative_weights=False)
+    #run_simulation_many_trials(utils.basic_hebb_update, "Basic Hebb")
+    #run_simulation_many_trials(utils.subtractive_normalization_update, "Subtractive Normalization")
+    #run_simulation_many_trials(utils.oja_update, "Oja", nonnegative_weights=False)
     
-    run_simulation_trajectory(utils.basic_hebb_update, "Basic Hebb")
-    run_simulation_trajectory(utils.subtractive_normalization_update, "Subtractive Normalization")
-    run_simulation_trajectory(utils.oja_update, "Oja", nonnegative_weights=False)
+    #run_simulation_trajectory(utils.basic_hebb_update, "Basic Hebb")
+    #run_simulation_trajectory(utils.subtractive_normalization_update, "Subtractive Normalization")
+    #run_simulation_trajectory(utils.oja_update, "Oja", nonnegative_weights=False)
+    
+    run_simulation_trajectory_average(utils.basic_hebb_averaged_update, "Basic Hebb")
+    run_simulation_many_trials_average(utils.basic_hebb_averaged_update, "Basic Hebb")
