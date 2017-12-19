ad7ed9c42ca25fc0eca5da39a9170c96aca04301
Cathy Chen <cathychen2357@gmail.com>
2017-12-19 11:24:22 -0500

    Update utils to handle recurrent connections.

--- ocular_dominance_simulation_multiple_neurons.py
+++ ocular_dominance_simulation_multiple_neurons.py
@@ -8,21 +8,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 import plasticity_simulations_utils as utils
 
-def recurrent_connections_simulation():
+# TODO: Create 2D simulation. Should be able to do it by making K matrix represent 2D Gaussian (using mod?) and reshaping weights array.
+# TODO: Put into jupyter notebook to make experimentation easier.
+def recurrent_connections_simulation_averaged(update_function):
     num_neurons = 500
     num_timesteps = 100
     num_inputs = 2
-    learning_rate = 0.01
+    learning_rate = 0.1
     weights = np.random.rand(num_neurons, num_inputs)
     
     Q = utils.generate_Q(num_inputs, num_timesteps)
     #K = utils.create_K(num_neurons)
     K = utils.create_K_Gaussian(num_neurons)
     for timestep in range(num_timesteps):
-        weights = weights + learning_rate + np.dot(np.dot(K, weights), Q)
+        weights = update_function(weights, learning_rate, Q, K)
+    
+    weights_diff = np.reshape(weights[:,0] - weights[:,1], (num_neurons, 1))
+    plt.imshow(np.transpose(weights_diff[:50,:]), cmap="gray")
+    
+def recurrent_connections_simulation(update_function):
+    num_neurons = 500
+    num_timesteps = 100
+    num_inputs = 2
+    learning_rate = 0.01
+    weights = np.random.rand(num_neurons, num_inputs)
+    
+    #K = utils.create_K(num_neurons)
+    K = utils.create_K_Gaussian(num_neurons)
+    for timestep in range(num_timesteps):
+        current_input = utils.generate_correlated_input()
+        weights = update_function(weights, current_input, learning_rate, K)
     
     weights_diff = np.reshape(weights[:,0] - weights[:,1], (num_neurons, 1))
     plt.imshow(np.transpose(weights_diff[:50,:]), cmap="gray")
     
 if __name__ == "__main__":
-    recurrent_connections_simulation()
+    #recurrent_connections_simulation_averaged(utils.basic_hebb_averaged_update)
+    recurrent_connections_simulation(utils.basic_hebb_update)
