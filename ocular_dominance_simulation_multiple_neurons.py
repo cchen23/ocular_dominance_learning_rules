795ac10c8bc8a2caeb20777e421adb74a78d1d13
Cathy Chen <cathychen2357@gmail.com>
2017-12-19 15:07:16 -0500

    Update utils to support more functions for multiple neurons.

--- ocular_dominance_simulation_multiple_neurons.py
+++ ocular_dominance_simulation_multiple_neurons.py
@@ -10,7 +10,7 @@ import plasticity_simulations_utils as utils
 
 # TODO: Create 2D simulation. Should be able to do it by making K matrix represent 2D Gaussian (using mod?) and reshaping weights array.
 # TODO: Put into jupyter notebook to make experimentation easier.
-def recurrent_connections_simulation_averaged(update_function):
+def recurrent_connections_simulation_averaged(update_function, update_name):
     num_neurons = 500
     num_timesteps = 100
     num_inputs = 2
@@ -24,13 +24,15 @@ def recurrent_connections_simulation_averaged(update_function):
         weights = update_function(weights, learning_rate, Q, K)
     
     weights_diff = np.reshape(weights[:,0] - weights[:,1], (num_neurons, 1))
+    plt.figure()
+    plt.title("Averaged Ocular Dominance Map, %s Update" % update_name)
     plt.imshow(np.transpose(weights_diff[:50,:]), cmap="gray")
     
-def recurrent_connections_simulation(update_function):
+def recurrent_connections_simulation(update_function, update_name):
     num_neurons = 500
     num_timesteps = 100
     num_inputs = 2
-    learning_rate = 0.01
+    learning_rate = 0.1
     weights = np.random.rand(num_neurons, num_inputs)
     
     #K = utils.create_K(num_neurons)
@@ -40,8 +42,11 @@ def recurrent_connections_simulation(update_function):
         weights = update_function(weights, current_input, learning_rate, K)
     
     weights_diff = np.reshape(weights[:,0] - weights[:,1], (num_neurons, 1))
+    plt.figure()
+    plt.title("Ocular Dominance Map, %s Update" % update_name)
     plt.imshow(np.transpose(weights_diff[:50,:]), cmap="gray")
     
 if __name__ == "__main__":
-    #recurrent_connections_simulation_averaged(utils.basic_hebb_averaged_update)
-    recurrent_connections_simulation(utils.basic_hebb_update)
+    #recurrent_connections_simulation_averaged(utils.basic_hebb_averaged_update, "Basic Hebb")
+    #recurrent_connections_simulation(utils.basic_hebb_update, "Basic Hebb")
+    recurrent_connections_simulation(utils.subtractive_normalization_update_multiple, "Subtractive Normalization")
--- plasticity_simulations_utils.py
+++ plasticity_simulations_utils.py
@@ -42,33 +42,49 @@ def create_K(num_neurons):
 # TODO: investigate effect of different sigmas.
 def create_K_Gaussian(num_neurons):
     K = np.empty((num_neurons, num_neurons))
-    sigma = 0.3
+    sigma = 0.4
     for i in range(num_neurons):
         for j in range(num_neurons):
             K[i,j] = 1/(sigma * np.sqrt(2 * np.pi)) * np.exp((-1/2) * ((i - j) / sigma)**2)
     return K
 
 # Update rules.
-def basic_hebb_update(weights, current_input, learning_rate):
-    activation = np.dot(weights, current_input)
-    activation = max(0, activation) # Prevent negative activity.
-    return weights + learning_rate * activation * current_input
+def compute_activation(weights, current_input, K):
+    if K is None:
+        activation = np.dot(weights, current_input)
+        activation = max(0, activation)
+    else:
+        activation = np.reshape(np.dot(K, np.dot(weights, current_input)), (K.shape[0], 1))
+        activation[activation < 0] = 0
+    return activation
 
-def subtractive_normalization_update(weights, current_input, learning_rate):
-    activation = np.dot(weights, current_input)
-    activation = max(0, activation) # Prevent negative activity.
-    n = np.ones(current_input.shape)
-    n[weights < 0] = 0
-    n_u = np.sum(n)
-    delta = activation * current_input - activation * np.dot(n, current_input) * n / n_u
-    return weights + learning_rate * delta
+def basic_hebb_update(weights, current_input, learning_rate, K=None):
+    activation = compute_activation(weights, current_input, K)
+    current_input = np.reshape(current_input, (1, current_input.shape[0]))
+    return weights + learning_rate * np.dot(activation, current_input)
     
-def oja_update(weights, current_input, learning_rate):
+def oja_update(weights, current_input, learning_rate, K=None):
+    activation = compute_activation(weights, current_input, K)
     alpha = 1
-    activation = np.dot(weights, current_input)
     activation = max(0, activation) # Prevent negative activity.
     delta = activation * current_input - alpha * activation * activation * weights
     return weights + learning_rate * delta
 
-def basic_hebb_averaged_update(weight, learning_rate, Q):
-     return weight + learning_rate * np.dot(Q, weight)
+def basic_hebb_averaged_update(weight, learning_rate, Q, K=None):
+    if K is None:
+        K = np.eye(weight.shape[0])
+    return weight + learning_rate * np.dot(K, np.dot(weight, Q))
+
+# Update rules, multiple neurons.
+def subtractive_normalization_update_multiple(weights, current_input, learning_rate, K):
+    activation = compute_activation(weights, current_input, K)
+    n = np.ones(current_input.shape)
+    #n[weights < 0] = 0
+    n_u = np.sum(n)
+    delta = np.dot(activation, np.reshape(current_input, (1, 2))) - activation * np.dot(n, current_input) * n / n_u
+    return weights + learning_rate * delta
+
+def competitive_hebb_update(weights, current_input, learning_rate, M):
+    activation = compute_activation_competitive_hebb(weights, current_input, M)
+    current_input = np.reshape(current_input, (1, current_input.shape[0]))
+    return weights + learning_rate * np.dot(activation, current_input)
